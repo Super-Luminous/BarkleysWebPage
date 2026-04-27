9d4b7c9a1c71588a05e9203a16087dc5bcfe0030
Super-Luminous <denis@superluminousmusic.com>
2026-04-27 14:41:12 -0500

    update

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,14 @@
 function setToViewportWidth(element)
 {
-    element.style.width = window.innerWidth
+
+    
+    var width = window.innerWidth;
+    document.getElementById("vpw").textContent = "View Port Width: " + width;
+    document.body.style.width = width + "px";
+}
+
+function setWidth(element, width)
+{
+    console.log("Attempt to set width of " + element + " to " + width)
+    element.style.width = width + "px";
 }
